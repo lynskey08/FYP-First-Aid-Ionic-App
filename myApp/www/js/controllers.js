fe5eeef869f2b5399e71322640033f0a44953d81
Gareth Lysnkey <lynskey123@gmail.com>
2016-04-13 19:38:58 +0100

    Added buttons for the emergency categories.Returned the php script in json format and displaying them as the button titles

--- myApp/www/js/controllers.js
+++ myApp/www/js/controllers.js
@@ -1,6 +1,18 @@
 angular.module('starter.controllers', [])
 
-.controller('EmergencyCtrl', function($scope) {})
+.controller('EmergencyCtrl', function($scope, $http) {
+	$scope.newTask = function() {
+        $http({
+            method: 'GET',
+            url: 'http://lynskey.cloudapp.net/emergencyCategories.php',
+        }).success(function(data) {
+            $scope.categories = data;
+        })
+          .error(function(data) {
+            $scope.categories = "No categories found by that name";
+        })
+    }
+})
 
 .controller('PrepareCtrl', function($scope) {})
 
